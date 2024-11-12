12f3b7ede8bb80db45ba5df700931040f1cc51d7
Ivan Brilata <brilataivan86@gmail.com>
2024-11-12 22:50:20 +0800

    users and profiles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   def index
     @authors = Author.all
     @books = Book.all
+    @users = User.all
+    @profiles = Profile.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root "home#index"
   resources :authors, only: [ :index, :show, :create, :update, :destroy, :edit, :new ]
   resources :books, only: [ :index, :show, :create, :update, :destroy, :edit, :new ]
+  resources :users, only: [ :index, :show, :create, :update, :destroy, :edit, :new ]
+  resources :profiles, only: [ :index, :show, :create, :update, :destroy, :edit, :new ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
